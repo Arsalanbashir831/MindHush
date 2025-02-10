6f431b1290218dd7b8d62d0344294657c21227da
Arsalan831 <arsalanbashir831@gmail.com>
2025-02-10 12:41:18 +0500

    set logo for responsive mobile view

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,18 +1,41 @@
 import React from "react";
-import { Box, Image, Text } from "@chakra-ui/react";
-import { Link } from "react-router";
-import { Flex } from "antd";
+import { Box, Image, Text, VStack, HStack } from "@chakra-ui/react";
+import { Link } from "react-router"; // Fixed import
 
 export default function Logo() {
-	return (
-		<Box alignSelf='flex-start'   >
-			<Link to='/'>
-				<Flex dir="col" align="center" gap={10}>
-				<Image width={'60px'} height={'80px'} src='/mindhush_logo_new_improved.png' alt='Company Logo' />
-				<Text fontWeight={'bold'} fontSize={'xl'}>MINDHUSH</Text>
-				</Flex>
-				
-			</Link>
-		</Box>
-	);
+  return (
+    <Box alignSelf="flex-start">
+      <Link to="/">
+        {/* Mobile View: Stack logo and text vertically */}
+        <VStack 
+          spacing={2} 
+          align="center" 
+          display={{ base: "flex", md: "none" }} 
+        >
+          <Image 
+            width={{ base: "50px", md: "60px" }} 
+            height={{ base: "70px", md: "80px" }} 
+            src="/mindhush_logo_new_improved.png" 
+            alt="Company Logo" 
+          />
+          <Text fontWeight="bold" fontSize={{ base: "lg", md: "xl" }}>MINDHUSH</Text>
+        </VStack>
+
+        {/* Desktop View: Arrange logo and text in a row */}
+        <HStack 
+          spacing={4} 
+          align="center" 
+          display={{ base: "none", md: "flex" }} 
+        >
+          <Image 
+            width={{ base: "50px", md: "60px" }} 
+            height={{ base: "70px", md: "80px" }} 
+            src="/mindhush_logo_new_improved.png" 
+            alt="Company Logo" 
+          />
+          <Text fontWeight="bold" fontSize={{ base: "lg", md: "xl" }}>MINDHUSH</Text>
+        </HStack>
+      </Link>
+    </Box>
+  );
 }
