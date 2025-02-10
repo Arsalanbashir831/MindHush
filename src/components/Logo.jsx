36c04dd46c851cc4df6a7339ef1016801973e1b8
Arsalan831 <arsalanbashir831@gmail.com>
2025-02-11 04:18:30 +0500

    update the logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,36 +1,20 @@
 import React from "react";
-import { Box, Image, Text, VStack, HStack } from "@chakra-ui/react";
+import { Box, Image, Text, HStack } from "@chakra-ui/react";
 import { Link } from "react-router"; // Fixed import
 
 export default function Logo() {
   return (
     <Box alignSelf="flex-start">
       <Link to="/">
-        {/* Mobile View: Stack logo and text vertically */}
-        <HStack 
-          spacing={2} 
-          align="center" 
-          display={{ base: "flex", md: "none" }} 
-        >
-	
-          <Image 
-             width={{ base: "70px", md: "70px" }} 
-			 height={{ base: "130px", md: "130px" }} 
-			src="/gradient_logo.png" 
-            alt="Company Logo" 
-          />
-          <Text fontWeight="bold" fontSize={{ base: "lg", md: "xl" }}>MINDHUSH</Text>
-        </HStack>
-
-        {/* Desktop View: Arrange logo and text in a row */}
-        <HStack 
-          spacing={4} 
-          align="center" 
-          display={{ base: "none", md: "flex" }} 
+        <HStack
+          spacing={2}
+          align="center"
+          flexDirection={{ base: "column", md: "row" }} // Vertical for mobile, horizontal for desktop
+          textAlign={{ base: "center", md: "left" }} // Center align text on mobile
         >
           <Image 
-            width={{ base: "70px", md: "70px" }} 
-            height={{ base: "70px", md: "130px" }} 
+            w={{ base: "80px", md: "80px" }} 
+            h="130px" 
             src="/gradient_logo.png" 
             alt="Company Logo" 
           />
